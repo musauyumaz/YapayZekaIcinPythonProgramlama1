8cc92f42fbb884e0c89c03cdf804d0be29b40492
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-13 20:16:06 +0300

    10. User Defined Functions

--- 1-Python Temelleri(Python Basics)/functions.py
+++ 1-Python Temelleri(Python Basics)/functions.py
@@ -8,3 +8,27 @@ round(float1)
 
 str2 = "1005"
 int(str2)
+
+# %% user defined functions
+var1 = 20
+var2 = 50
+output = (((var1 + var2)*50)/100.0) * var1 / var2
+
+#fonksiyon parametresi = input
+def benim_ilk_func(a, b):
+    """
+    bu benim ilk denemem
+
+    parametre:
+    
+    return:
+    """
+    output = (((a + b)*50)/100.0) * a / b
+    return output
+
+benim_ilk_func(1, 2)
+sonuc = benim_ilk_func(var1, var2)
+
+def deneme1(): 
+    print("bu benim ikinci denemem")
+
