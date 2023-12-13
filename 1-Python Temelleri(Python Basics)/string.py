9d056acfc25e8689bfe0b9a0a6bd86107f0a5d18
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-13 19:47:48 +0300

    7. String

---
+++ 1-Python Temelleri(Python Basics)/string.py
@@ -0,0 +1,22 @@
+# %%
+# string
+s = "bugün günlerden pazartesi"
+variable_type = type(s) # str = string
+var1 = 10
+type(var1)
+
+print(s)
+
+var1 = "ankara"
+var2 = "ist"
+var3 = var1+var2
+
+var4 = "100"
+var5 = "200"
+var6 = var4+var5
+
+uzunluk = len(var6)
+
+# var6[3]
+
+# %%
