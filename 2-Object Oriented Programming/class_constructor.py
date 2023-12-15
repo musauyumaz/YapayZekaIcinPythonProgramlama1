a27bd83b8f11d09fe60cde5f21502ef2e7b9d19c
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-15 19:39:11 +0300

    23. Class Variables

--- 2-Object Oriented Programming/class_constructor.py
+++ 2-Object Oriented Programming/class_constructor.py
@@ -1,19 +1,34 @@
 class Calisan:
+    
+    zamOrani = 1.8
+    counter = 0
     def __init__(self,isim,soyisim,maas):
         self.isim = isim
         self.soyisim = soyisim
         self.maas = maas
         self.email = isim+soyisim+"@asd.com"
+        
+        Calisan.counter = Calisan.counter + 1
     
     def giveNameSurname(self):
         return self.isim + " " + self.soyisim
+    
+    def zamYap(self):
+        self.maas = self.maas + self.maas * self.zamOrani
         
-isci1 = Calisan("Ali", "Veli", 100)
+# isci1 = Calisan("Ali", "Veli", 100)
+# isci1
+# isci1.isim
+# isci1.email
+# isci1.giveNameSurname()
 
-isci1
+# %% class variables
 
-isci1.isim
+calisan1 = Calisan("Ali", "Veli", 100)
+print("ilk maas: ", calisan1.maas)
+calisan1.zamYap()
+print("yeni maas: ", calisan1.maas)
 
-isci1.email
+calisan2 = Calisan("Ayse", "Tınmaz", 200)
 
-isci1.giveNameSurname()
+calisan2.counter
