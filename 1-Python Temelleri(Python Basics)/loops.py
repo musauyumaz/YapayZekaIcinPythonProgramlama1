8e56b8da2e593fd61a4877dc74c81100efedec91
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-15 19:09:34 +0300

    20. While Loop

--- 1-Python Temelleri(Python Basics)/loops.py
+++ 1-Python Temelleri(Python Basics)/loops.py
@@ -19,3 +19,16 @@ for each in liste:
     
 print(count)
     
+
+# while loop
+i = 0
+while i < 4:
+    print(i)
+    i = i + 1
+    
+sinir = len(liste)
+each = 0
+count = 0
+while (each < sinir):
+    count = count + liste[each]
+    each = each + 1
