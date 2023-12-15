cf5007623e6f540a54d3131e943a9dc9376d1798
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-15 20:43:08 +0300

    27. Try - Except 1

--- 3-Dealing With Programming Errors/error.py
+++ 3-Dealing With Programming Errors/error.py
@@ -10,3 +10,25 @@ i = 0
 while (i < 10):
     print(i)
     i = i + 1
+    
+# %% exceptions
+a = 10
+b = "2"
+liste = [1,2,3]
+print(sum(liste))
+# print(a + b)
+print(str(a) + b)
+k = 10
+print(k)
+zero = 0
+# a = k/zero # hata
+
+if(zero == 0):
+    a = 0
+else:
+    a = k / zero
+    
+try:
+    a = k/zero
+except ZeroDivisionError:
+    a= 0
