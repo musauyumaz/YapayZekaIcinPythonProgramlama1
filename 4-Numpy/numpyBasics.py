c86748145142f19b3e583a8ca69b854e4b03d95b
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-16 12:32:16 +0300

    30. Numpy Basic Operations

--- 4-Numpy/numpyBasics.py
+++ 4-Numpy/numpyBasics.py
@@ -29,3 +29,47 @@ print(a)
 
 a = np.linspace(10, 50,20)
 print(a)
+
+
+# %% numpy basic operations
+
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+print(a+b)
+print(a-b)
+print(a**2)
+
+print(np.sin(a))
+
+
+print(a < 2)
+
+a = np.array([[1,2,3], [4,5,6]])
+b = np.array([[1,2,3], [4,5,6]])
+
+# element wise product
+print(a * b)
+
+# matrix product
+a.dot(b.T)
+
+print(np.exp(a))
+
+a = np.random.random((5,5))
+
+print(a.sum())
+print(a.max())
+print(a.min())
+
+
+print(a.sum(axis=0))
+print(a.sum(axis = 1))
+
+
+print(np.sqrt(a))
+print(np.square(a))
+
+
+np.add(a,a)
+
+
