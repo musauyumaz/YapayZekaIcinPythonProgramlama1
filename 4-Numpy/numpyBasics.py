29201823d80c025c207baac404eb442e3730790e
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-16 12:45:37 +0300

    31. Indexing and Slicing

--- 4-Numpy/numpyBasics.py
+++ 4-Numpy/numpyBasics.py
@@ -72,4 +72,28 @@ print(np.square(a))
 
 np.add(a,a)
 
+# %% indexing and slicing
+import numpy as np
+array = np.arange(1,8)
+
+print(array[0])
+
+print(array[:4])
+
+reverse_array = array[::-1]
+print(reverse_array)
+
+array1 = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+
+print(array1[1,1])
+
+print(array1[:,1])
+
+print(array1[1,1:4])
+
+print(array1[-1,:])
+print(array1[:,-1])
+
+
+
 
