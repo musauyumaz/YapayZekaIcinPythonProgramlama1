d69288b8eac6dfd9ffdadeffada1ff85235524de
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-16 20:52:58 +0300

    39. List Comprehension

--- 5-Pandas/pandas.py
+++ 5-Pandas/pandas.py
@@ -52,3 +52,15 @@ filtre2 = dataframe1.AGE < 20
 dataframe1[filtre1 & filtre2]
 
 dataframe1[dataframe1.AGE > 60]
+
+# %% list comprehension
+import numpy as np
+#ortalama_maas = dataframe1.MAAS.mean()
+
+#ortalama_maas_np = np.mean(dataframe1.MAAS)
+
+dataframe1["maas_seviyesi"] = ["düşük" if ortalama_maas > i else "yüksek" for i in dataframe1.maas]
+
+dataframe1.columns = [i.lower() for i in dataframe1.columns]
+
+dataframe1.columns = [i.split()[0] + "_" + i.split()[1] if len(i.split()) > 1 else i for i in dataframe1.columns]
