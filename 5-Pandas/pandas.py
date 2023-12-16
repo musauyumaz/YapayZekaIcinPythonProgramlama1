954649c4af0e4003cf9eba749dbb39fd48ef1959
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-16 20:05:06 +0300

    36. Pandas Basic Methods

--- 5-Pandas/pandas.py
+++ 5-Pandas/pandas.py
@@ -10,3 +10,13 @@ dataframe1 = pd.DataFrame(dictionary)
 
 head = dataframe1.head()
 tail = dataframe1.tail()
+
+# %% pandas basic method
+
+print(dataframe1.columns)
+
+print(dataframe1.info())
+
+print(dataframe1.dtypes)
+
+print(dataframe1.describe()) # numeric feature columns (age, maas)
