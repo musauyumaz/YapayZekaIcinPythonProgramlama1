2c7dc4980e72d0f64bcceb5fbe75c0e787676ed1
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-17 09:16:25 +0300

    43. Line Plot

--- 6-Visualization with Matplotlib/pandasReviewAndMatplot.py
+++ 6-Visualization with Matplotlib/pandasReviewAndMatplot.py
@@ -17,3 +17,27 @@ versicolor = df[df.Species == "Iris-versicolor"]
 
 print(setosa.describe())
 print(versicolor.describe())
+
+# %% 
+import matplotlib.pyplot as plt
+
+df1 = df.drop(["Id"],axis=1)
+
+# df1.plot()
+# plt.show()
+
+setosa = df[df.Species == "Iris-setosa"]
+versicolor = df[df.Species == "Iris-versicolor"]
+virginica = df[df.Species == "Iris-virginica"]
+
+plt.plot(setosa.Id,setosa.PetalLengthCm, color = "red", label = "setosa", grid = True)
+plt.plot(versicolor.Id,versicolor.PetalLengthCm, color = "green", label = "versicolor")
+plt.plot(virginica.Id,virginica.PetalLengthCm, color = "blue", label = "virginica")
+plt.xlabel("Id")
+plt.ylabel("PetalLengthCm")
+plt.legend()
+plt.show()
+
+df1.plot(grid=True, linestyle = ":")
+df1.plot(grid=True, alpha = 0.1)
+plt.show()
