33cf0cf2bf3dc64c53312faa5eaef4295ca66479
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-17 09:22:02 +0300

    44. Scatter Plot

--- 6-Visualization with Matplotlib/pandasReviewAndMatplot.py
+++ 6-Visualization with Matplotlib/pandasReviewAndMatplot.py
@@ -41,3 +41,20 @@ plt.show()
 df1.plot(grid=True, linestyle = ":")
 df1.plot(grid=True, alpha = 0.1)
 plt.show()
+
+# %% scatter plot
+
+setosa = df[df.Species == "Iris-setosa"]
+versicolor = df[df.Species == "Iris-versicolor"]
+virginica = df[df.Species == "Iris-virginica"]
+
+plt.scatter(setosa.PetalLengthCm, setosa.PetalWidthCm, color="red", label="setosa")
+plt.scatter(versicolor.PetalLengthCm, versicolor.PetalWidthCm, color="green", label="versicolor")
+plt.scatter(virginica.PetalLengthCm, virginica.PetalWidthCm, color="blue", label="virginica")
+plt.legend()
+plt.xlabel("PetalLengthCm")
+plt.xlabel("PetalWidthCm")
+plt.title("scatter plot")
+plt.show()
+
+
