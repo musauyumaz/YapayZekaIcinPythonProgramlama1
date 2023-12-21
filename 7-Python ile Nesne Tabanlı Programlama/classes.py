61f54b966ae399d9246681fa1e80969e754c8299
musauyumaz <113431738+musauyumaz@users.noreply.github.com>
2023-12-21 14:57:37 +0300

    52. Metotlar

--- 7-Python ile Nesne Tabanlı Programlama/classes.py
+++ 7-Python ile Nesne Tabanlı Programlama/classes.py
@@ -34,3 +34,27 @@ print(f1.football_club)
 
 f1.football_club = "real madrid"
 print(f1.football_club)
+
+# %% methods
+class Square(object):
+    
+    edge = 5 # meter
+    area = 0
+    
+    def area1(self):
+        self.area = self.edge * self.edge
+        print("Area: ",self.area)
+    
+    
+s1 = Square()
+print(s1)
+print(s1.edge)
+print(s1.area1())
+
+s1.edge
+s1.area
+
+s1.edge = 7
+s1.area1()
+
+
