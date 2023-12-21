d2c9d16f85b024fcb34614b2690de4d2e60efc5b
musauyumaz <113431738+musauyumaz@users.noreply.github.com>
2023-12-21 14:10:35 +0300

    50. Sınıflar/Classes

---
+++ 7-Python ile Nesne Tabanlı Programlama/classes.py
@@ -0,0 +1,19 @@
+# integer = 10
+# string = "messi" "barcelona football team"
+# %% 
+integer1 = 33
+string1 = "messi"
+
+# %% classes
+
+employee1Name = "messi"
+employee1Age = 33
+employee1Address = "asdasdad"
+
+class Employee(object):
+    # attribute = age, address, name
+    # behaviour = pass
+    pass
+
+
+employee1 = Employee()
