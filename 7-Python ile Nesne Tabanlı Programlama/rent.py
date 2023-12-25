eb7a88a0d4c54fda801eae6b5e8df8cf087e0c2d
musauyumaz <113431738+musauyumaz@users.noreply.github.com>
2023-12-25 17:13:43 +0300

    66. Araç Kiralama Sınıfı (Parent Class)

--- 7-Python ile Nesne Tabanlı Programlama/rent.py
+++ 7-Python ile Nesne Tabanlı Programlama/rent.py
@@ -1,18 +1,91 @@
+import datetime
+
+
 class VehicleRent:
     def __init__(self, stock):
-        pass
+        self.stock = stock
+        self.now = 0
 
     def displayStock(self):
-        pass
+        print("Vehicle avaliable to rent: ", self.stock)
+        return self.stock
 
     def rentHourly(self, n):
-        pass
+        if n <= 0:
+            print("Number should be positive")
+            return None
+        elif n > self.stock:
+            print(f"Sorry {self.stock} vehicle available to rent")
+            return None
+        else:
+            self.now = datetime.datetime.now()
+            print(f"Rented a {n} vehicle for hourly at {self.now.hour} hours")
+
+            self.stock -= n
+            return self.now
 
     def rentDaily(self, n):
-        pass
+        if n <= 0:
+            print("Number should be positive")
+            return None
+        elif n > self.stock:
+            print(f"Sorry {self.stock} vehicle available to rent")
+            return None
+        else:
+            self.now = datetime.datetime.now()
+            print(f"Rented a {n} vehicle for daily at {self.now.hour} hours")
+
+            self.stock -= n
+            return self.now
 
     def returnVehicle(self, request, brand):
-        pass
+        carHPrice = 10
+        carDPrice = carHPrice * 8 / 10 * 24
+        bikeHPrice = 5
+        bikeDPrice = bikeHPrice * 7 / 10 * 24
+
+        rentalTime, rentalBasis, numOfVehicle = request
+        bill = 0
+
+        if brand == "car":
+            if rentalTime and rentalBasis and numOfVehicle:
+                self.stock += numOfVehicle
+                now = datetime.datetime.now()
+                rentalPeriod = now - rentalTime
+
+                if rentalBasis == 1:
+                    bill = rentalPeriod.seconds/3600*carHPrice*numOfVehicle
+                elif rentalBasis == 2:
+                    bill = rentalPeriod.seconds/3600*24*carDPrice*numOfVehicle
+
+                if 2 <= numOfVehicle:
+                    print("You have extra 20% discount")
+                    bill = bill * 0.8
+
+                print("Thank you for returning your car")
+                print(f"Price: ${bill}")
+                return bill
+
+        elif brand == "bike":
+            if rentalTime and rentalBasis and numOfVehicle:
+                self.stock += numOfVehicle
+                now = datetime.datetime.now()
+                rentalPeriod = now - rentalTime
+
+                if rentalBasis == 1:
+                    bill = rentalPeriod.seconds/3600*bikeHPrice*numOfVehicle
+                elif rentalBasis == 2:
+                    bill = rentalPeriod.seconds/3600*24*bikeDPrice*numOfVehicle
+
+                if 4 <= numOfVehicle:
+                    print("You have extra 20% discount")
+                    bill = bill * 0.8
+
+                print("Thank you for returning your bike")
+                print(f"Price: ${bill}")
+                return bill
+        else:
+            print("You do not rent a vehicle")
 
 
 class CarRent(VehicleRent):
