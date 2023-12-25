f9e5c879f581726cb8b7a0ce9fa0f6020818f344
musauyumaz <113431738+musauyumaz@users.noreply.github.com>
2023-12-25 15:51:05 +0300

    61. Overriding

---
+++ 7-Python ile Nesne Tabanlı Programlama/overriding.py
@@ -0,0 +1,14 @@
+class Animal:
+    def toString(self):
+        print("Animal")
+
+
+class Monkey(Animal):
+    def toString(self):
+        print("Monkey")
+
+a1 = Animal()
+a1.toString()
+
+m1 = Monkey()
+m1.toString()
