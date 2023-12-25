6b236f255d9adbcb9cb92730cb56b0a5761f0123
musauyumaz <113431738+musauyumaz@users.noreply.github.com>
2023-12-25 17:44:08 +0300

    71. Müşteri Giriş Sayfası: Araba Menüsü

--- 7-Python ile Nesne Tabanlı Programlama/main.py
+++ 7-Python ile Nesne Tabanlı Programlama/main.py
@@ -50,7 +50,8 @@ while True:
                 mainMenu = True
                 print("----------------------")
             elif choice == 4:
-                bike.returnVehicle(customer.returnVehicle("bike"), "bike")
+                customer.bill = bike.returnVehicle(
+                    customer.returnVehicle("bike"), "bike")
                 customer.rentalBasisB, customer.rentalTimeB, customer.bikes = 0, 0, 0
                 mainMenu = True
             elif choice == 5:
@@ -59,5 +60,57 @@ while True:
                 break
             else:
                 print("Invalid Input please enter a number 1-6")
+                mainMenu = True
+        elif choice == "B" or choice == "b":
+            print("""
+                  ***** CAR MENU *****
+                  1.Display available cars
+                  2.Request a car on hourly basics $ 10
+                  3.Request a car on daily basics $ 192
+                  4.Return a car
+                  5.Main Menu
+                  6.Exit
+                  """)
+            choice = input("Enter choice")
 
+            try:
+                choice: int(choice)
+            except ValueError:
+                print("It is not integer")
+                continue
 
+            if choice == 1:
+                car.displayStock()
+                choice = "B"
+            elif choice == 2:
+                customer.rentalTimeC = car.rentHourly(
+                    customer.requestVehicle("car"))
+                customer.rentalBasisC = 1
+                mainMenu = True
+                print("----------------------")
+            elif choice == 3:
+                customer.rentalTimeC = car.rentDaily(
+                    customer.requestVehicle("car"))
+                customer.rentalBasisC = 2
+                mainMenu = True
+                print("----------------------")
+            elif choice == 4:
+                customer.bill = car.returnVehicle(
+                    customer.returnVehicle("car"), "car")
+                customer.rentalBasisC, customer.rentalTimeC, customer.cars = 0, 0, 0
+                mainMenu = True
+            elif choice == 5:
+                mainMenu = True
+            elif choice == 6:
+                break
+            else:
+                print("Invalid Input please enter a number 1-6")
+                mainMenu = True
+        if choice == "Q" or choice == "q":
+            break
+        else:
+            print("Invalid input Please Enter A-B-Q")
+            mainMenu = True
+        print("Thank you for using the cehicle rental shop")
+                
+        
