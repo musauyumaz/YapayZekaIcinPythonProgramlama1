c176231c9d4cdf5e58d2322c05f2c7013d45fd49
musauyumaz <113431738+musauyumaz@users.noreply.github.com>
2023-12-25 16:39:34 +0300

    65. Araç Kiralama Projesinde Kullanılan Sınıflar ve Metotlar

---
+++ 7-Python ile Nesne Tabanlı Programlama/rent.py
@@ -0,0 +1,40 @@
+class VehicleRent:
+    def __init__(self, stock):
+        pass
+
+    def displayStock(self):
+        pass
+
+    def rentHourly(self, n):
+        pass
+
+    def rentDaily(self, n):
+        pass
+
+    def returnVehicle(self, request, brand):
+        pass
+
+
+class CarRent(VehicleRent):
+    def __init__(self):
+        pass
+
+    def displayStock(self):
+        pass
+
+
+class BikeRent(VehicleRent):
+
+    def __init__(self):
+        pass
+
+
+class Customer:
+    def __init__(self):
+        pass
+
+    def requestVehicle(self):
+        pass
+
+    def returnVehicle(self):
+        pass
